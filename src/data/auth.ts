bc94d0fa5bb3c075f8431c3b9fbc0186d24c91ef
Ardanila <ardanila@gmail.com>
2025-01-19 13:52:11 +0300

    Initial commit

--- src/data/auth.ts
+++ src/data/auth.ts
@@ -1,3 +1,5 @@
+import { setCurrentUser } from './storage';
+
 export const checkCredentials = (login: string, password: string): boolean => {
   return login === import.meta.env.VITE_REACT_APP_LOGIN && password === import.meta.env.VITE_REACT_APP_PASSWORD;
 };
@@ -9,8 +11,12 @@ export const isAuthenticated = (): boolean => {
 
 export const setAuthenticated = (value: boolean): void => {
   sessionStorage.setItem('isAuthenticated', value.toString());
+  if (value) {
+    setCurrentUser({ login: import.meta.env.VITE_REACT_APP_LOGIN });
+  }
 };
 
 export const clearAuth = (): void => {
   sessionStorage.removeItem('isAuthenticated');
+  setCurrentUser(null);
 }; 
