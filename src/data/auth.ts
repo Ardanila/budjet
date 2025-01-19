4c2e23a8976d11227658bd2a363627a0eefdc255
Ardanila <ardanila@gmail.com>
2025-01-19 09:44:55 +0300

    Fix: remove unused config import

--- src/data/auth.ts
+++ src/data/auth.ts
@@ -1,5 +1,3 @@
-import config from '../config/auth.json';
-
 export const checkCredentials = (login: string, password: string): boolean => {
   return login === process.env.REACT_APP_LOGIN && password === process.env.REACT_APP_PASSWORD;
 };
